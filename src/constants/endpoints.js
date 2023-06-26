6d2ee85fe9c016e7d7e0b5a86f3ee594c15b25fd
Ubuntu <ubuntu@ip-172-31-5-236.ap-northeast-2.compute.internal>
2023-06-26 17:15:19 +0000

    Modifying references for prod

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,4 +1,5 @@
-export const ROOT_TEST_URL="http://localhost:5000";
+//export const ROOT_TEST_URL="http://localhost:5000";
+export const ROOT_TEST_URL="https://api.treehouse.contextural.io";
 export const PLATFORM_INFO = "/platform_info";
 export const REGISTRY_ASPECTS = "/registry/all"
 export const REGISTRY_ASPECTS_OBJECT = "/registry/all?format=object"
